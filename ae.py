686f7a6a3a5633947bdcc86198d954d5ef26a12a
Kartik Sharma <kash5740@colorado.edu>
2024-04-03 17:17:12 -0600

    Tested Keras Summary Of Encoder

--- ae.py
+++ ae.py
@@ -1,5 +1,6 @@
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization, Flatten, Dense
+from tensorflow.keras import backend as K
 
 class Autoencoder:
     '''
@@ -18,13 +19,17 @@ class Autoencoder:
         self.model = None #tensor flow model
 
         self._num_conv_layers = len(conv_filters) #number of convolution filters
+        self._shape_before_bottleneck = None
 
         self._build() #will build encoder, decoder, autoencoder
     
+    def summary(self):
+        self.encoder.summary()
+
     def _build(self):
         self._build_encoder()
-        self._build_decoder()
-        self._build_autoencoder()
+        #self._build_decoder()
+        #self._build_autoencoder()
 
     def _build_encoder(self):
         encoder_input = self._add_encoder_input()
@@ -49,3 +54,38 @@ class Autoencoder:
         """
         adds a convolution block to a graph of layers, consisting of conv 2d  + ReLu + batch_normalization layer
         """
+        layer_number = layer_index + 1
+        conv_layer = Conv2D(
+            filters = self.conv_filters[layer_index],
+            kernel_size = self.conv_kernels[layer_index],
+            strides = self.conv_strides[layer_index],
+            padding="same",
+            name = f"encoder_conv_layer_{layer_number}"
+        )
+        x = conv_layer(x)
+        x = ReLU(name = f"encoder_relu_{layer_number}")(x)
+        x = BatchNormalization(name = f"encoder_bn_{layer_number}")(x)
+        return x
+
+    def _add_bottleneck(self, x):
+        '''
+        Flatten the data and add bottleneck (Dense layer).
+        '''
+        self._shape_before_bottleneck = K.int_shape(x)[1:] #shape of the data before we flatten
+        x = Flatten()(x)
+        x = Dense(self.latent_space_dim, name="encoder_output")(x)
+
+        return x
+
+
+if __name__ == "__main__":
+    autoencoder = Autoencoder(
+        input_shape= (28,28,1),
+        conv_filters=(32, 64, 64,64),
+        conv_kernels=(3,3,3,3),
+        conv_strides=(1,2,2,1),
+        latent_space_dim=2
+    )
+
+    autoencoder.summary()
+    
